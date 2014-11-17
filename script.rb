bc39269b2ba1cc96ef4c362234212497d201eed6
r00t <r00t@Macintosh.local>
2014-11-17 21:10:17 +0100

    V0.1

--- script.rb
+++ script.rb
@@ -3,17 +3,35 @@ require 'data_mapper'
 require 'dm-sqlite-adapter'
 #
 #Set path to DB
-DataMapper.setup(:default, 'sqlite:///Users/r00t/Documents/database.db')
+DataMapper.setup(:default, 'sqlite:///Users/r00t/Documents/fcms.db')
 #########################
 class Post
     include DataMapper::Resource
     #define Properties
     property :id,           Serial
     property :title,        String
-    property :body,         Text
+    property :subtitle,     String
+    property :content,      Text
     property :created_at,   DateTime
+    property :created_by,   String
+    
+    has n, :comments
 #End Class
 end
+
+class Comment
+    include DataMapper::Resource
+    #define Properties
+    property :id,           Serial
+    property :title,        String
+    property :content,      Text
+    property :created_at,   DateTime
+    property :created_by,   String
+    
+    belongs_to :post
+#End Class
+end
+    
 #
 #update Model-Relations
 DataMapper.finalize
