2963109e483da50ec6ad6586023077d5edc20878
fahloi <fahrni.olivier@gmail.com>
2014-12-01 20:50:39 +0100

    v0.2

--- app.rb
+++ app.rb
@@ -1,4 +1,68 @@
-#include needed gems
 require 'sinatra'
-require 'sinatra/cookies'
-
+require 'data_mapper'
+require 'rack-flash'
+require 'dm-sqlite-adapter'
+#
+enable :sessions
+use Rack::Flash
+#
+DataMapper.setup(:default, 'sqlite:///Users/r00t/Documents/test.db')
+#
+class User
+    include DataMapper::Resource
+    property :id,       Serial
+    property :name,     String
+    property :password, BCryptHash
+end
+#
+get '/register' do
+    erb :register
+end
+#
+post '/register' do
+    u = params[:name]
+    p = params[:pwrd]
+    p2 = params[:pwrd2]
+    #
+    if p != p2
+        flash[:errorMessage] = "Passwords don't match"
+        redirect '/register'
+    else
+        User.create(name: u, password: p)
+        flash[:msg] = 'Hallo ' + u
+        redirect '/login'
+    end
+end
+#
+get '/login' do
+    erb :backend
+end
+#
+get '/protected' do
+    if session[:user_id]
+        @uName = User.first(session[:user_id]).name
+        erb :protected
+    else
+        redirect '/login'
+    end
+end
+#
+post '/login' do
+    u = User.first(name: params[:name])
+    if u
+        if u.password == params[:password]
+            session[:user_id] = u.id
+            flash[:msg] = 'Hallo ' + u.name
+            redirect '/protected'
+        else
+            flash[:errorMessage] = 'Wröng passw0rd'
+            redirect '/login'
+        end
+    else
+        flash[:errorMessage] = 'U hav n0 ak0unt'
+        redirect '/login'
+    end
+end
+#
+DataMapper.finalize
+DataMapper.auto_upgrade!
